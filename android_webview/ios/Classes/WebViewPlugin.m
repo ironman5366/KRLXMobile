c7d38bb1ce08594c8441ba24c74682d048b95c77
Will Beddow <will.beddow@gmail.com>
2019-05-20 16:29:00 -0500

    Basic iOS compatibility work

---
+++ android_webview/ios/Classes/WebViewPlugin.m
@@ -0,0 +1,20 @@
+#import "WebViewPlugin.h"
+
+@implementation WebViewPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"android_webview"
+            binaryMessenger:[registrar messenger]];
+  WebViewPlugin* instance = [[WebViewPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
